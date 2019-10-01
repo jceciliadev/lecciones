40a88ec07ee25d54c8367ec3387d32dc5932c03f
janeth <vargasjaneth984@gmail.com>
2019-10-01 20:10:29 +0200

    String

--- lecion/projects/logging/src/main/java/Switch.java
+++ lecion/projects/logging/src/main/java/Switch.java
@@ -1,6 +1,8 @@
 public class Switch {
     public static void main(String[] args) {
         System.out.println("uno mas uno es dos");
-        
+         String nombre = new String("eeee");
+         String x = "weeee";
+
     }
 }
